f8875031662cef9d61170a94776b021fa6b45f29
dikimeip <pm159145@gmail.com>
2019-12-17 15:37:20 +0700

    membuat logic login siakad

--- application/controllers/LoginController.php
+++ application/controllers/LoginController.php
@@ -10,5 +10,17 @@ class LoginController extends CI_Controller
 	{
 		$this->load->view('login');
 	}
+
+	public function DoLogin()
+	{
+		$id = $this->input->post('level');
+		if ($id == 0) {
+			echo "Student";
+		} elseif ($id == 1) {
+			echo "Teacher";
+		} elseif ($id == 2) {
+			echo "Staff";
+		}
+	}
 	
 }
