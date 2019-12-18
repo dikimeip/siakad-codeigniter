767b6617814ca53844c8685d30db93098c10fada
dikimeip <pm159145@gmail.com>
2019-12-18 15:02:21 +0700

    membuat logout dan middleware login

--- application/controllers/SiswaController.php
+++ application/controllers/SiswaController.php
@@ -10,15 +10,24 @@ class SiswaController extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->model('MyModel','Models');
+		if ($this->session->userdata('isSiswa') == "" ) {
+			redirect('LoginController/index');
+		}
 	}
 
 	public function index()
 	{
 		$data['sess'] = $this->session->userdata('isSiswa');
-		$this->load->view('siswa/template/header');
+		$this->load->view('siswa/template/header',$data);
 		$this->load->view('siswa/template/menu');
 		$this->load->view('siswa/dasboard',$data);
 		$this->load->view('siswa/template/footer');
 
+	}
+
+	public function logout()
+	{
+		$this->session->unset_userdata('isSiswa');
+		redirect('LoginCOntroller/index');
 	}
 }
