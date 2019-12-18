c86fc328d5f16aa0db309ba61e979def2edf1375
dikimeip <pm159145@gmail.com>
2019-12-18 15:23:25 +0700

    instalasi template di halaman guru

---
+++ application/views/guru/template/footer.php
@@ -0,0 +1,40 @@
+</div>
+   </div>
+      </div>
+        </div>
+          </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2019 <a href="https://www.templatewatch.com/" target="_blank">Templatewatch</a>. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="<?php echo base_url() ?>asset/vendors/base/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <script src="<?php echo base_url() ?>asset/vendors/chart.js/Chart.min.js"></script>
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="<?php echo base_url() ?>asset/js/off-canvas.js"></script>
+  <script src="<?php echo base_url() ?>asset/js/hoverable-collapse.js"></script>
+  <script src="<?php echo base_url() ?>asset/js/template.js"></script>
+  <script src="<?php echo base_url() ?>asset/js/todolist.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="<?php echo base_url() ?>asset/js/dashboard.js"></script>
+  <!-- End custom js for this page-->
+</body>
+
+</html>
+
---
+++ application/views/guru/template/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>RoyalUI Admin</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/css/style.css">
+  <!-- endinject -->
+ 
+</head>
+
+          
+                
