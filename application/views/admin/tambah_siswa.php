6219d0f9019651e7a850c83b8405f53eb9c044b6
dikimeip <pm159145@gmail.com>
2019-12-20 14:48:44 +0700

    membuat form tambah data siswa

--- application/views/admin/tambah_siswa.php
+++ application/views/admin/tambah_siswa.php
@@ -1,1 +1,30 @@
 <h1 class="text-center">TAMBAH SISWA</h1>
+<form action="" method="post" enctype="multipart/form-data">
+	<div class="form-group">
+		<label>Masukan Nim</label>
+		<input type="text" name="nim" class="form-control">
+	</div>
+	<div class="form-group">
+		<label>Masukan Nama</label>
+		<input type="text" name="nama" class="form-control">
+	</div>
+	<div class="form-group">
+		<label>Masukan Kelas</label>
+		<select class="form-control" name="kelas">
+			<option>9</option>
+		</select>
+	</div>
+	<div class="form-group">
+		<label>Masukan Tahun Masuk</label>
+		<input type="text" name="tahun" class="form-control">
+	</div>
+	<div class="form-group">
+		<label>Masukan Alamat</label>
+		<input type="text" name="alamat" class="form-control">
+	</div>
+	<div class="form-group">
+		<label>Masukan Foto</label>
+		<input type="file" name="foto" class="form-control">
+	</div>
+	<input type="submit" value="SIMPAN" class="btn btn-info">
+</form>
