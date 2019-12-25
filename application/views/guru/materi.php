5107cc6d6608eea7ad545f709fff2dd6b9294545
dikimeip <pm159145@gmail.com>
2019-12-25 08:27:19 +0700

    membuat form tambah data materi

--- application/views/guru/materi.php
+++ application/views/guru/materi.php
@@ -2,7 +2,7 @@
 <br>
 <div class="row">
 	<div class="col-md-8">
-		<button class="btn btn-info">TAMBAH</button>
+		<button class="btn btn-info" data-toggle="modal" data-target="#tambah">TAMBAH</button>
 	</div>
 	<div class="col-md-4">
 		<form>
@@ -10,5 +10,31 @@
 				<input type="text" name="cari" class="form-control" placeholder="Masukan Nama Materi">
 			</div>
 		</form>
+	</div>
+</div>
+<div class="modal fade" id="tambah">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<b>TAMBAH MATERI</b>
+			</div>
+			<div class="modal-body">
+				<form>
+					<div class="form-group">
+						<label>Masukan Nama Materi</label>
+						<input type="text" name="nama" class="form-control">
+					</div>
+					<div class="form-group">
+						<label>Masukan Deskripsi Materi</label>
+						<textarea class="form-control" name="desk"></textarea>
+					</div>
+					<div class="form-group">
+						<label>Masukan File</label>
+						<input type="file" name="file" class="form-control">
+					</div>
+					<input type="submit" value="SIMPAN" class="btn btn-success">
+				</form>
+			</div>
+		</div>
 	</div>
 </div>
