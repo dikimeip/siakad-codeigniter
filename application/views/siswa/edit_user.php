0e246777c81874921ff1796c7575466364fdaf3d
dikimeip <pm159145@gmail.com>
2019-12-29 08:55:06 +0700

    membuat form ubah data siswa

--- application/views/siswa/edit_user.php
+++ application/views/siswa/edit_user.php
@@ -1,1 +1,18 @@
 <h1 class="text-center">EDIT SISWA</h1>
+<br>
+<form action="<?php echo base_url('') ?>" method="post" enctype="multipart/form-data">
+	<div class="form-group">
+		<label>Nama Siswa</label>
+		<input required="" type="text" name="nama" class="form-control">
+	</div>
+	<div class="form-group">
+		<label>Alamat Siswa</label>
+		<input required=""  type="text" name="alamat" class="form-control">
+	</div>
+	<div class="form-group">
+		<label>Foto Guru</label>
+		<img style="width: 100px" src="<?php echo base_url() ?>asset/image/"><br>
+		<input type="file" name="foto" class="form-control">
+	</div>
+	<input type="submit" name="foto" class="btn btn-info">
+</form>
